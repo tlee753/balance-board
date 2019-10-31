1e7ad09bb3065ce73cd8fd93c3f669f6635862eb
Tyler Lee <tlee753@gmail.com>
2019-10-31 12:33:16 -0400

    updates

---
+++ wii-scale/CMakeFiles/wii-scale.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tyler/Github/balance-board/wii-scale/BlueZDevice.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/BlueZDevice.cpp.o"
+  "/home/tyler/Github/balance-board/wii-scale/UDev.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/UDev.cpp.o"
+  "/home/tyler/Github/balance-board/wii-scale/UDevDevice.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/UDevDevice.cpp.o"
+  "/home/tyler/Github/balance-board/wii-scale/XWiiIface.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/XWiiIface.cpp.o"
+  "/home/tyler/Github/balance-board/wii-scale/XWiiMonitor.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/XWiiMonitor.cpp.o"
+  "/home/tyler/Github/balance-board/wii-scale/wii-scale.cpp" "/home/tyler/Github/balance-board/wii-scale/CMakeFiles/wii-scale.dir/wii-scale.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/giomm-2.4"
+  "/usr/lib/x86_64-linux-gnu/giomm-2.4/include"
+  "/usr/include/libmount"
+  "/usr/include/blkid"
+  "/usr/include/glibmm-2.4"
+  "/usr/lib/x86_64-linux-gnu/glibmm-2.4/include"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/sigc++-2.0"
+  "/usr/lib/x86_64-linux-gnu/sigc++-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
